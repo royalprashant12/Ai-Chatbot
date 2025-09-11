7c8c66facb0607ea34a883ed760b226cb16bfbb2
PrashantKumar5201283 <prashantroyal18@gmail.com>
2025-09-11 22:40:28 +0530

    my smart chatbot

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import { generate } from "./routes/chatRoutes.js";
+import { config } from "dotenv";
+config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
